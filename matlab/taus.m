c929be3507a397dfe5a83445a827bdb1ef5d0271
anaamansari <anaamansari@hotmail.com>
2016-03-21 12:48:47 -0700

    taus URNG generator

---
+++ matlab/taus.m
@@ -0,0 +1,38 @@
+%Anaam Ansari
+%
+% seed_1= fi(0,0,32,0);
+% seed_1.hex='67580';
+function [y]= taus(s,N)
+    s0= fi(0,0,32,0);
+    s1= fi(0,0,32,0);
+    s2= fi(0,0,32,0);
+    
+    m1=fi(0,0,32,0);
+    m1.hex='10850089';
+    m2=fi(0,0,32,0);
+    m2.hex='89305309';
+    
+    
+    mask0= fi(0,0,32,0);
+    mask1= fi(0,0,32,0);
+    mask2= fi(0,0,32,0);
+    
+    mask1.hex='FFFFFFFE';
+    mask2.hex='FFFFFFF8';
+    mask3.hex='FFFFFFF0';
+    
+    s0=s;
+    s1= bitxor(m1,s0);
+    s2 =bitxor(m2,s0);  
+    for i=1:N
+        b= bitsrl(bitxor(bitsll(s0,13),s0),19);
+        s0= bitxor(bitsll(bitand(s0,mask0),12), b);
+        b= bitsrl(bitxor(bitsll(s1,2),s1),25);
+        s1= bitxor(bitsll(bitand(s1,mask1),4), b);
+        b= bitsrl(bitxor(bitsll(s2,3),s2),11);
+        s2= bitxor(bitsll(bitand(s2,mask2),17), b);
+        temp=bitxor(s0,s1);
+        y(i)=bitxor(s2,temp);
+    end
+    
+end
